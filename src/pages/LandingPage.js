3e94bd9a8371aea3dca8f7023e179c90699e2f5d
VinsDev <vinsdev185@gmail.com>
2024-04-08 19:37:09 +0100

    name

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 const LandingPage = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Our Library App</h1>
+      <h1 className="text-3xl font-bold mb-4">Welcome to the FCS Library</h1>
       <p className="text-lg text-gray-700 mb-8">
-        This is the landing page of our library application.
+        This is the landing page of library website. Details about the site coming soon . . .
       </p>
       <Link to="/login">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
